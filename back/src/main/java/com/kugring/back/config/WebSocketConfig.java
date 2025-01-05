de4fce32f1a6ae37fb48be1e9fc7d5967a864e8b
kugring <wogur8243@gmail.com>
2025-01-06 00:57:58 +0900

    웹소켓 테스트중

--- back/src/main/java/com/kugring/back/config/WebSocketConfig.java
+++ back/src/main/java/com/kugring/back/config/WebSocketConfig.java
@@ -4,36 +4,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 import com.kugring.back.handler.MyWebSocketHandler;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer  {
-
-    // @Override
-    // public void configureMessageBroker(MessageBrokerRegistry config) {
-    //     config.enableSimpleBroker("/topic", "/order", "/pointCharge", "/user", "/manager", "/cashPay");  // /topic 경로 추가
-    //     config.setApplicationDestinationPrefixes("/app");
-    // }
-
-    // @SuppressWarnings("null")
-    // @Override
-    // public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-    //     registry.addHandler(myHandler(), "/ws").setAllowedOrigins("*");
-    // }
-
+@SuppressWarnings("null")
+public class WebSocketConfig implements WebSocketConfigurer, WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerStompEndpoints(@SuppressWarnings("null") StompEndpointRegistry registry) {
-    registry.addEndpoint("/ws").setAllowedOrigins("*");
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/ws")
+                .setAllowedOrigins("https://hyunam.site", "http://localhost:3000");
     }
 
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("https://hyunam.site", "http://localhost:3000")
+                .withSockJS(); // withSockJS()로 SockJS를 사용
+    }
 
     public WebSocketHandler myHandler() {
         return new MyWebSocketHandler();
     }
-
-
 }
