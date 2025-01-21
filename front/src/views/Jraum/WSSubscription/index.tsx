4ea73767bb64f82fd9b787904d5e83f358db5f1a
kugring <wogur8243@gmail.com>
2025-01-22 00:00:31 +0900

    새로운 방법으로 IOS에서 음악 재생 시도해보기

--- front/src/views/Jraum/WSSubscription/index.tsx
+++ front/src/views/Jraum/WSSubscription/index.tsx
@@ -1,5 +1,5 @@
 import { TEST_DOMAIN } from 'constant';
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import usePointChargeStore from 'store/modal/point-charge-modal.store';
 import useWebSocketStore from 'store/web-socket.store';
 
@@ -31,81 +31,118 @@ const WSSubscription = () => {
         });
     };
 
+
     //          state: 웹소켓에서 받아올 음성을 저장할 객체 상태                //
     const audioRef = useRef<HTMLAudioElement | null>(null);
+    const [audioSrc, setAudioSrc] = useState<string | null>(null);
 
-    const audioContext = new (window.AudioContext)();
+
+    // 추가
+    const playFakeAudio = useCallback(() => {
+        if (audioRef.current) {
+            audioRef.current.src = "/dingdong.mp3";  // public 디렉토리에 있는 dingdong.mp3 파일을 로드
+            audioRef.current.muted = true;
+            audioRef.current.onplay = null;
+            audioRef.current.onended = null;
+            setTimeout(() => {
+                audioRef.current!.muted = false;
+                audioRef.current!.onplay = null;
+            }, 100);
+            setTimeout(() => {
+                audioRef.current!.muted = true;
+                audioRef.current!.onplay = null;
+            }, 1800);
+        }
+    }, []);
+
+
+    const fetchAudioSrc = useCallback((orderId: number) => {
+        playFakeAudio(); // 추가
+        setTimeout(async () => {
+            // TTS 음성을 불러옵니다.
+            const response = await fetch(`${TEST_DOMAIN}/api/v1/order/${orderId}/audio`);
+            const audioBlob = await response.blob();
+            const audioUrl = URL.createObjectURL(audioBlob);
+            setAudioSrc(audioUrl);
+        }, 2000);
+    }, []);
+
+    useEffect(() => {
+        if (audioRef.current && audioSrc) {
+            audioRef.current.src = audioSrc;
+            audioRef.current.muted = false; // 추가
+            audioRef.current.onplay = () => {
+                console.log('Audio playback started');
+            };
+            audioRef.current.onended = () => {
+                console.log('Audio playback finished');
+            };
+        }
+    }, [audioSrc]);
+
 
     //          function: 주문 음성 듣기 함수               //
-    const actionTTS = async (orderId: number) => {
-        try {
-            const existingMediaElements = document.querySelectorAll<HTMLMediaElement>('audio, video');
-            existingMediaElements.forEach((mediaElement) => {
-                if (!mediaElement.paused) {
-                    mediaElement.pause();
-                }
-            });
-            console.log("이건 audioRef", audioRef.current);
-            // 먼저 dingdong.mp3를 재생
-            if (audioRef.current) {
-                const source = audioContext.createMediaElementSource(audioRef.current);
-                source.connect(audioContext.destination);
+    // const actionTTS = async (orderId: number) => {
+    //     try {
+    //         const existingMediaElements = document.querySelectorAll<HTMLMediaElement>('audio, video');
+    //         existingMediaElements.forEach((mediaElement) => {
+    //             if (!mediaElement.paused) {
+    //                 mediaElement.pause();
+    //             }
+    //         });
+    //         console.log("이건 audioRef", audioRef.current);
+    //         // 먼저 dingdong.mp3를 재생
+    //         if (audioRef.current) {
+    //             console.log("scr를 dingdong.mp3로 바꿈");
+    //             audioRef.current.src = '/dingdong.mp3';  // public 디렉토리에 있는 dingdong.mp3 파일을 로드
+    //             console.log("플레이 하기 전");
+    //             audioRef.current.play();
+    //             console.log("플레이 후");
+    //             // dingdong 소리가 끝난 후에 TTS를 재생
+    //             audioRef.current.onended = async () => {
+    //                 console.log('Dingdong sound finished. Now playing TTS.');
 
-                // 사용자 상호작용으로 오디오 컨텍스트를 활성화
-                if (audioContext.state === 'suspended') {
-                    await audioContext.resume();
-                }
+    //                 // TTS 음성을 불러옵니다.
+    //                 const response = await fetch(`${TEST_DOMAIN}/api/v1/order/${orderId}/audio`);
+    //                 const audioBlob = await response.blob();
+    //                 const audioUrl = URL.createObjectURL(audioBlob);
 
-                // 기존 dingdong.mp3를 재생
-                audioRef.current.src = '/dingdong.mp3';
-                await audioRef.current.play().catch((error) => {
-                    console.error("Dingdong playback error:", error);
-                });
+    //                 if (audioRef.current) {
+    //                     audioRef.current.src = audioUrl;
+    //                     audioRef.current.oncanplaythrough = () => {
+    //                         console.log('TTS loaded successfully');
+    //                         audioRef.current?.play().catch((error) => {
+    //                             console.error('TTS playback error:', error);
+    //                         });
+    //                     };
 
-                audioRef.current.onended = async () => {
-                    console.log('Dingdong sound finished. Now playing TTS.');
+    //                     audioRef.current.onended = () => {
+    //                         console.log('TTS finished, resuming previous audio.');
+    //                         existingMediaElements.forEach((mediaElement) => {
+    //                             if (mediaElement.dataset.wasPlaying === 'true') {
+    //                                 mediaElement.play().catch((error) => {
+    //                                     console.error('Error resuming playback:', error);
+    //                                 });
+    //                             }
+    //                         });
+    //                     };
 
-                    // TTS 음성을 불러옵니다.
-                    const response = await fetch(`${TEST_DOMAIN}/api/v1/order/${orderId}/audio`);
-                    const audioBlob = await response.blob();
-                    const audioUrl = URL.createObjectURL(audioBlob);
-
-                    if (audioRef.current) {
-                        audioRef.current.src = audioUrl;
-
-                        // 처음에는 무음으로 설정
-                        audioRef.current.muted = true;
-
-                        audioRef.current.oncanplaythrough = () => {
-                            console.log('TTS loaded successfully');
-                            // 재생 시작
-                            audioRef.current!
-                                .play()
-                                .then(() => {
-                                    // 재생이 성공하면 음소거 해제
-                                    audioRef.current!.muted = false;
-                                })
-                                .catch((error) => {
-                                    console.error('TTS playback error:', error);
-                                });
-                        };
-
-                        audioRef.current.onerror = (error) => {
-                            console.error('Audio playback error:', error);
-                        };
-                    }
-                };
-            }
-        } catch (error) {
-            console.error('Error fetching or playing audio:', error);
-        }
-    };
+    //                     audioRef.current.onerror = (error) => {
+    //                         console.error('Audio playback error:', error);
+    //                     };
+    //                 }
+    //             };
+    //         }
+    //     } catch (error) {
+    //         console.error('Error fetching or playing audio:', error);
+    //     }
+    // };
 
     //              function: 사용자 주문 웹소켓 구독 핸들러               // 
     const OrderTTSSubscribe = () => {
         manager?.subscribe('/receive/user/orderTTS', (orderId) => {
             console.log(orderId);
-            actionTTS(orderId);
+            fetchAudioSrc(orderId);
         });
     };
 
@@ -142,7 +179,9 @@ const WSSubscription = () => {
 
     //              render: 키오스크 웹소켓 렌더링              //
     return (
-        <></>
+        <>
+            <audio autoPlay hidden ref={audioRef} /> {/* autoPlay 추가 */}
+        </>
     );
 };
 
